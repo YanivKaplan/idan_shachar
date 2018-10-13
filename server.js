a768073e8fdcc1004ecd5725622c2f84053d58b0
יניב קפלן <syaniv19@gmail.com>
2018-10-13 23:39:14 +0300

    fixes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/idan-shachar-conversations'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/idan-shachar-conversations/index.html'));
+res.sendFile(path.join(__dirname+'/dist/idan-shachar-conversations/app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
